dc2564d37958ec8091d011701b7f779ed258c0df
Christian Haynes <Haynesc3419@gmail.com>
2022-12-22 16:54:39 -0500

    3

--- AJH/settings.py
+++ AJH/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# Secret Key REMOVED for github
+SECRET_KEY = 'django-insecure-lu2u7xixlkwfw%515%-d%#2*)a^8^mxp$781bx7-%isd5iso@9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -144,9 +144,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/Assignments'
 
 EMAIL_BACKEND = 'django_ses.SESBackend'
-# EMAIL_HOST = **REMOVED**
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-# AWS_ACCESS_KEY_ID = **REMOVED**
-# AWS_SECRET_ACCESS_KEY = **REMOVED**
+AWS_ACCESS_KEY_ID = 'AKIAVXD7TGYO26EWO6DY'
+AWS_SECRET_ACCESS_KEY = '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'
 AWS_DEFAULT_ACL = None
